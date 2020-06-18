9f2e1ff394f64c8f0ff22619a2bab009c68574f9
Thibaut Callabat <63956366+TCallabat@users.noreply.github.com>
2020-06-18 19:43:45 +0200

    .

---
+++ build/precache-manifest.a794243615d5d6a6efc2eddc35df7d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d79728d50a022ebf90e0ce09125c53b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1461e1955936c60c1aae",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8c8bdcf58ea31456c244",
+    "url": "/portfolio/static/css/main.abce2c5a.chunk.css"
+  },
+  {
+    "revision": "1461e1955936c60c1aae",
+    "url": "/portfolio/static/js/2.0fcb0c85.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.0fcb0c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c8bdcf58ea31456c244",
+    "url": "/portfolio/static/js/main.df526ec5.chunk.js"
+  },
+  {
+    "revision": "208f80b7e73ac1ae3492",
+    "url": "/portfolio/static/js/runtime-main.ae347c8b.js"
+  }
+]);
