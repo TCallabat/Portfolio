67877489cdfb8f7afcfc7552994fa208fdd3e415
Thibaut Callabat <63956366+TCallabat@users.noreply.github.com>
2020-06-18 19:40:06 +0200

    Your awesome message

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,52 +4,52 @@
 
 /* imports modules  */
 import React, { } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./style.scss";
 
 /* component */
-function Navbar(props) {
+function Header(props) {
 
     /* return */
     return (
-        <Container fluid className="m-0 p-0 mainContainer bg-warning" id="home">
-            <nav className="navbar navbar-expand-md navbar-dark fixed-top">
-                <button className="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav m-auto">
-                        <li className="navbar-brand text-dark">Thibaut Callabat</li>
-                        <li>
-                            <Link to="/">accueil</Link>
-                        </li>
-                        <li>
-                            <Link to="about">a propos</Link>
-                        </li>
-                        <li>
-                            <Link to="skills">compétences</Link>
-                        </li>
-                        <li>
-                            <Link to="projects">projets</Link>
-                        </li>
-                        <li>
-                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/TCallabat">
-                                <img src="./bxl-github.svg" alt="github" />
-                            </a>
-                        </li>
-                        <li>
-                            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/thibaut-callabat/">
-                                <img src="./bxl-linkedin.svg" alt="linkedin" />
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+        <Container fluid className="m-0 p-0" id="home">
+                <Navbar expand="lg" className="m-0 p-0 w-100">
+                <Navbar.Brand href="/" className="name ml-auto">Thibaut Callabat</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <ul className="navbar-nav">
+                                <li>
+                                    <Link to="/">accueil</Link>
+                                </li>
+                                <li>
+                                    <Link to="about">a propos</Link>
+                                </li>
+                                <li>
+                                    <Link to="skills">compétences</Link>
+                                </li>
+                                <li>
+                                    <Link to="projects">projets</Link>
+                                </li>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/TCallabat">
+                                        <img src="./bxl-github.svg" alt="github" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/thibaut-callabat/">
+                                        <img src="./bxl-linkedin.svg" alt="linkedin" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>         
         </Container >
     );
 }
 
 /* export */
-export default Navbar;
+export default Header;
