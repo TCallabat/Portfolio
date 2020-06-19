beb0268cd8c5c9fd42c62a00e7db518d5cd0f109
Thibaut Callabat <63956366+TCallabat@users.noreply.github.com>
2020-06-19 13:11:43 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e67372ef35edd10bb85b32c4e884ef51.js"
+  "/portfolio/precache-manifest.85f059a3aeb1d0b3c3281b95f62ae290.js"
 );
 
 self.addEventListener('message', (event) => {
